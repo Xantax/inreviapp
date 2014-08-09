1961926296f8b06758ebc399bb38f5d40d3ddd97
Xantax <xantar@ymail.com>
2014-08-09 17:26:37 +0000

    add google analytics

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20140807225050) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.integer  "credit",                 default: 10
+    t.integer  "credit",                 default: 100
     t.text     "bio"
     t.string   "website"
     t.string   "language"
@@ -60,12 +60,12 @@ ActiveRecord::Schema.define(version: 20140807225050) do
     t.datetime "last_seen_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",  null: false
+    t.string   "encrypted_password",     default: "",  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,   null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"

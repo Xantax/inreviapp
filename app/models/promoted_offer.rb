ee6f138725ce027552a7815acc3f83ae01b3e510
Xantax <xantar@ymail.com>
2014-08-20 23:26:50 +0000

    credit bug 10

--- app/models/promoted_offer.rb
+++ app/models/promoted_offer.rb
@@ -4,24 +4,6 @@ class PromotedOffer < ActiveRecord::Base
   
   scope :published, -> { where('clicks <= set_clicks') }
   
-  # Find PromotedOffers created by current_user
-  scope :promos_by_cu, ->(user_id) { where(:user_id => User.current.id)}
-  
-  # Array of :set_clicks values
-  def self.get_clickz
-    self.promos_by_cu(User.current.id).map{ |v| v.set_clicks }
-  end
-  
-  # Sum of all set_clicks values for the current_user
-  def self.sum_of_all
-    self.get_clickz.inject{|sum,x| sum + x }
-  end
-  
-  # Total Incoins
-#  def self.tot_sum
-#    ((User.current.credit.to_i) - (self.sum_of_all.to_i))
-#  end
-  
-  validates :set_clicks, numericality: { only_integer: true, :greater_than => 0, :less_than_or_equal_to => (User.creditz) }
+  validates :set_clicks, numericality: { only_integer: true, :greater_than => 0, :less_than_or_equal_to => (user.credit) }
     
 end

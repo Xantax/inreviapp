e12ca3a2afa0cc2454ebec970d79731986c6c6c8
Xantax <xantar@ymail.com>
2014-08-21 13:55:03 +0000

    credit bug 36

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,8 @@ class User < ActiveRecord::Base
     Thread.current[:user] = user
   end
   
-  def self.yoyo
-    ((self.current[:credit] + (50*24)).to_i
+  def yoyo
+    yoyo = ((self.current[:credit] + (50*24)).to_i
   end
   
   has_many :offers do

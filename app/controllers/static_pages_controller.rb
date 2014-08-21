b66bcf58abce8d17d5c1ae535d609ad014994b79
Xantax <xantar@ymail.com>
2014-08-21 02:26:23 +0000

    credit bug 22

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_user!, only: [:buycredit]
   
   def home
-    if user_signed_in?
-    @promoted_offers = PromotedOffer.published.order('set_clicks DESC').paginate(:page => params[:page], :per_page => 20)
-    end
+#    if user_signed_in?
+#    @promoted_offers = PromotedOffer.published.order('set_clicks DESC').paginate(:page => params[:page], :per_page => 20)
+#    end
   end
   
   def terms

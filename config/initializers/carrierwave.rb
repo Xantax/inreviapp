6e54ca4e4ffd29e62a030951593afd7acc5a57b6
Xantax <xantar@ymail.com>
2014-08-28 22:55:04 +0000

    bug cdn ser

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,6 @@ CarrierWave.configure do |config|
     aws_access_key_id: 'AKIAIGCJKAUC3OHB6ATA',
     aws_secret_access_key: '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'
   }
+  config.fog_host = "d29puu9ttvyqql.cloudfront.net"
   config.fog_directory = 'inreviapp'
 end

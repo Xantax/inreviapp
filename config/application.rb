d9bc66418e184282145e885bc30c3a1f013f520b
Xantax <xantar@ymail.com>
2014-10-01 01:53:44 +0000

    glyphicons bug 4

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Inreviapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.assets.paths << Rails.root.join( 'vendor', 'assets' )
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.woff *.eot *.svg *.ttf)
   end
 end
